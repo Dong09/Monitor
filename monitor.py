35d57ca0240f8e41916ebb3b81bcf99caeeaa003
Dong09 <45942487+Dong09@users.noreply.github.com>
2021-05-25 11:39:54 +0800

    first commit

---
+++ monitor.py
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+import cv2
+
+
+def init_camera():
+    try:
+        capture = cv2.VideoCapture(0)
+        while(True):
+            # 获取一帧
+            ret, frame = capture.read()
+            # 将这帧转换为灰度图
+            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('frame', frame)
+            # 如果输入q，则退出
+            if cv2.waitKey(1) == ord('q'):
+                break
+    except e:
+        print(e)
+
+
+init_camera()
+=======
+print('cc')
+>>>>>>> 809832607626e516cf5426da0ac256eb5dec4e5c
